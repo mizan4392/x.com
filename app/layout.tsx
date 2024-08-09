2123e85c4935330fd100e8c1a79d06d56e5026ac
green <macbook@macs-MacBook-Pro.local>
2024-08-09 02:04:24 +0200

    news

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,7 +24,14 @@ export default function RootLayout({
             <SideBar />
           </div>
           <div> {children}</div>
-          <div>
+          <div className="lg:flex-col p-3 h-screen border-l hidden lg:flex w-[24rem]">
+            <div className=" sticky top-0 bg-white py-2 ">
+              <input
+                placeholder="Search"
+                type="text"
+                className=" bg-gray-100 border-l border-gray-200 rounded-3xl text-sm w-full px-4 py-2"
+              />
+            </div>
             <News />
           </div>
         </div>
--- components/News.tsx
+++ components/News.tsx
@@ -1,5 +1,63 @@
-import React from "react";
+"use client";
 
+import Image from "next/image";
+import React, { useEffect, useState } from "react";
+type Article = {
+  source: {
+    id: string | null;
+    name: string;
+  };
+  author: string;
+  title: string;
+  description: string;
+  url: string;
+  urlToImage: string;
+  publishedAt: string;
+  content: string;
+};
 export default function News() {
-  return <div>News</div>;
+  const [news, setNews] = useState<Article[]>([]);
+  const [articleNumber, setArticleNumber] = useState(3);
+
+  useEffect(() => {
+    fetch("https://saurav.tech/NewsAPI/top-headlines/category/business/us.json")
+      .then((res) => res.json())
+      .then((data) => setNews(data.articles));
+  }, []);
+
+  return (
+    <div className=" text-gray-700 space-y-3 bg-gray-100 rounded-xl pt-2">
+      <h1 className="text-xl font-bold  px-4">Current News</h1>
+      {news.slice(0, articleNumber).map((article, i) => {
+        return (
+          <div key={article.title} className="p-3 border-b">
+            <a href={article.url} target="_blank">
+              <div className=" flex items-center justify-between px-4 py-2 space-x-1 hover:bg-gray-200 transition duration-200">
+                <div className=" space-y-0.5">
+                  <h6 className=" text-sm font-bold ">{article.title}</h6>
+                  <p className=" text-xs font-medium text-gray-500">
+                    {article.source.name}
+                  </p>
+                </div>
+                {article.urlToImage?.length && (
+                  <img
+                    src={article.urlToImage}
+                    alt={String(i)}
+                    width={70}
+                    className="rounded-xl"
+                  />
+                )}
+              </div>
+            </a>
+          </div>
+        );
+      })}
+      <button
+        className="text-blue-300 pl-4 pb-3 hover:text-blue-400 text-sm"
+        onClick={() => setArticleNumber(articleNumber + 3)}
+      >
+        Load More
+      </button>
+    </div>
+  );
 }
