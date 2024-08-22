6e1b99a872b9dd8cedd2ff662c5f54fe417daca2
green <macbook@macs-MacBook-Pro.local>
2024-08-22 19:42:51 +0200

    added post component

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,13 @@
+import Input from "@/components/Input";
 import Image from "next/image";
 
 export default function Home() {
-  return <main>Home</main>;
+  return (
+    <div className=" max-w-xl mx-auto border-l border-r  min-h-screen">
+      <div className="py-2 px-3 sticky top-0 z-50 bg-white border-b border-gray-200">
+        <h2 className=" text-lg sm:text-xl font-bold ">Home</h2>
+      </div>
+      <Input />
+    </div>
+  );
 }
