073226a9ce086ef6b4e92f125ffcef059549e66f
green <macbook@macs-MacBook-Pro.local>
2024-08-26 15:05:32 +0200

    firebase setup and upload images

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,4 @@
 import Input from "@/components/Input";
-import Image from "next/image";
 
 export default function Home() {
   return (
--- components/Input.tsx
+++ components/Input.tsx
@@ -1,11 +1,66 @@
 "use client";
+import { app } from "@/firebase";
+import {
+  getDownloadURL,
+  getStorage,
+  ref,
+  uploadBytesResumable,
+} from "firebase/storage";
 import { useSession } from "next-auth/react";
-import React from "react";
+import React, { ChangeEvent, useEffect } from "react";
 import { HiOutlinePhotograph } from "react-icons/hi";
 
 export default function Input() {
   const { data: session } = useSession();
+  const [imageUrl, setImageUrl] = React.useState<string>("");
+  const [selectedImage, setSelectedImage] = React.useState<File | null>(null);
+  const [imageUploading, setImageUploading] = React.useState<boolean>(false);
+  const inputRef = React.useRef<HTMLInputElement>(null);
 
+  useEffect(() => {
+    if (selectedImage) {
+      handelUploadImageToServer();
+    }
+  }, [selectedImage]);
+
+  const handelUploadImageToServer = async () => {
+    if (!selectedImage) return;
+    setImageUploading(true);
+    const store = getStorage(app);
+    const fileName = new Date().getTime() + "-" + selectedImage?.name;
+    const storageRef = ref(store, fileName);
+    const uploadTask = uploadBytesResumable(storageRef, selectedImage);
+    uploadTask.on(
+      "state_changed",
+      (snapshot) => {
+        const progress =
+          (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+        console.log("Upload is " + progress + "% done");
+      },
+      (error) => {
+        console.log(error);
+        setImageUploading(false);
+        setSelectedImage(null);
+        setImageUrl("");
+      },
+      () => {
+        getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+          console.log("File available at", downloadURL);
+          setImageUploading(false);
+
+          setImageUrl(downloadURL);
+        });
+      }
+    );
+  };
+
+  const handleImageUpload = (e: ChangeEvent<HTMLInputElement>) => {
+    const file = e.target.files?.[0];
+    if (file) {
+      setSelectedImage(file);
+      setImageUrl(URL.createObjectURL(file));
+    }
+  };
   if (!session) return null;
   return (
     <div className="flex border-b border-gray-200 p-3 space-x-3 w-full">
@@ -20,8 +75,25 @@ export default function Input() {
           placeholder="Whats happening"
           rows={2}
         ></textarea>
+        {selectedImage && (
+          <img
+            src={imageUrl}
+            alt="uplodedImage"
+            className="w-full max-h-[250px] object-cover cursor-pointer"
+          />
+        )}
         <div className="flex items-center justify-between pt-2.5">
-          <HiOutlinePhotograph className="h-10 w-10 p-2 text-sky-500 hover:bg-sky-100 rounded-full cursor-pointer" />
+          <HiOutlinePhotograph
+            onClick={() => inputRef?.current?.click()}
+            className="h-10 w-10 p-2 text-sky-500 hover:bg-sky-100 rounded-full cursor-pointer"
+          />
+          <input
+            type="file"
+            accept="image/*"
+            ref={inputRef}
+            onChange={handleImageUpload}
+            className="hidden"
+          />
           <button className="bg-blue-400 text-white px-4 py-1.5 rounded-full font-bold shadow-md hover:brightness-95 disabled:opacity-50">
             Post
           </button>
