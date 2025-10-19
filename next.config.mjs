c43b10394ef6c40240bbd2925cbfdaf509ee7d93
“Mizan” <macbook@macs-MacBook-Pro.local>
2025-10-19 16:31:46 +0200

    updated next conf v5

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+      basePath: '/app1',
+  assetPrefix: '/app1/',
+  output: 'standalone', // recommended for Docker
 };
 
 export default nextConfig;
