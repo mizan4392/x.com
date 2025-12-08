516b2296a12df1f01ac110f861f06c907cdd8968
“Mizan” <macbook@macs-MacBook-Pro.local>
2025-12-08 23:23:24 +0100

    conf fix

--- next.config.mjs
+++ next.config.mjs
@@ -4,6 +4,10 @@ const nextConfig = {
   // assetPrefix: '/app1',
   // // Optional: if you're using basePath (recommended for subpath deployments)
   // basePath: '/app1',
+
+  basePath: "/social",
+  assetPrefix: "/social",
+
 };
 
 export default nextConfig;
